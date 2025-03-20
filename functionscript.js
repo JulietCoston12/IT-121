c8706df28459294873a460d6cfd108def68d5acf
JulietCoston12 <155699040+JulietCoston12@users.noreply.github.com>
2025-03-19 21:40:06 -0700

    Add files via upload

---
+++ functionscript.js
@@ -0,0 +1,41 @@
+function  validateform () {
+  let Firstname = "Lastname,password,message"
+document.getElementById("Firstname");
+// add validatedPassword function here
+console.log("message") ;
+//UserActivationvalidatedForm (myform) => {
+// return firsname.value.length;  
+document.getElementById("demo").innerHTML =("validatedForm").length; 
+  console.log(ValidatedForm);
+}
+
+function validatepassword(){
+let password =  document.getElementById("password").value;
+
+ // validatePassword should return true if a password is valid, and false otherwise.
+//Invoke this new function from validateForm function,
+let  length = password.length;// ('Function', 'Validated', 'Form');
+// thatGuy.print();
+let Firstname =,  Lastname,  message, passwoerd = "myForm";
+console.log()("message");
+// add validatedPassword function here
+console.log("password") ;
+//UserActivationvalidatedForm (myform) => {
+
+ myform.firstname.value; // get text of firstname field
+
+  // update the "message" Div with the approriate wording
+
+document.getElementById("message").innerHTML = Firstname.toString('...');
+console.log(Firstname);
+console.log(Lastname)
+  $("message").HTML("buttom clicked");
+
+  for(let i  = 0; i <str.lenght; i++) {
+  if ("Firstname, Lastname, message, password");
+  string.paasword.indexOf(Str[i]  =-1) 
+     return false;
+  } document.querySelectorAll("Firsrname, Lastname, message, password");
+    
+  }
+  
