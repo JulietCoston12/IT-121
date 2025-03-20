834d6d701c1f296068ca9008bbd65243931a0165
JulietCoston12 <155699040+JulietCoston12@users.noreply.github.com>
2025-03-19 22:50:13 -0700

    Add files via upload

---
+++ gamescript.js
@@ -0,0 +1,14 @@
+const cards = ['Haert', 'Spade', 'Diamon', 'Club']
+
+
+functionng();
+
+
+oor(Math.random() * 52); 
+console.log(Hearts); // return array of cards in order
+console.log(Spades.lenght); // return number of cards in deck
+console.log(diamond.lenght[0]); // retun first card in deck
+console.log(suit[clubs.lenght -1]); // return last card in the deck
+console.log(cards[math.floor(math.random() * 52)]) // return random card in the deck
+
+
