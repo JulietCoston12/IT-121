a684e7f77dd08f46482652b51a70c4e928203b95
JulietCoston12 <155699040+JulietCoston12@users.noreply.github.com>
2025-03-20 01:35:46 -0700

    Add files via upload

---
+++ weatherscript.js
@@ -0,0 +1,14 @@
+function translatePigLatin(str){
+
+}
+// var variableName = variablevalue;
+var vowel = "aeiou";
+vowel.search("a");
+vowel.search("e");
+vowel.search("i");
+vowel.search("o");
+vowel.search("u");
+
+
+console.log(translatePigLatin("a"));
+console.log(vowel.search("e"));
